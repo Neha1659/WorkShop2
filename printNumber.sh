7065cd8222243121d6a75f3e6884254d7764df7e
Neha1659 <nehareddyanthireddy@gmail.com>
2023-03-07 09:13:04 +0530

    Add: Print selected values

--- printNumber.sh
+++ printNumber.sh
@@ -1,6 +1,19 @@
 i=1
 while [ $i -le 10 ]
 do 
-   echo $i
-   i=$(($i+1))
+  case $i in
+  1) echo $i
+  ;;
+  2) echo $i
+  ;;
+  4) echo $i
+  ;;
+  7) echo $i
+  ;;
+  9) echo $i
+  ;;
+  10) echo $i
+  ;;
+  esac  
+  i=$(($i+1))
 done
